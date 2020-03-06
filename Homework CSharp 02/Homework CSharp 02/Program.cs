987bdb59c5cfb1c14b7851bb950a2484a97192fc
Petar Gjorgjioski <58377922+LordGrim15@users.noreply.github.com>
2020-03-07 00:57:37 +0100

    C# Homework #02
    Second C# Homework

---
+++ Homework CSharp 02/Homework CSharp 02/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Homework_CSharp_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number: ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter a second number: ");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            if(number1 > number2)
+            {
+                if (number1 % 2 == 0)
+                {
+                    Console.WriteLine($"The larger number is {number1} and the number is even!");
+                }
+                else
+                {
+                    Console.WriteLine($"The larger number is {number1} and the number is odd!");
+                }
+            }
+            else
+            {
+                if (number2 % 2 == 0)
+                {
+                    Console.WriteLine($"The larger number is {number2} and the number is even!");
+                }
+                else
+                {
+                    Console.WriteLine($"The larger number is {number2} and the number is odd!");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
