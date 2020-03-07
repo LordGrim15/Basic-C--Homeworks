c87139e226a8836f3af2a8f4e2de7e5631d955a2
Petar Gjorgjioski <58377922+LordGrim15@users.noreply.github.com>
2020-03-07 01:28:33 +0100

    C# Homework 2-2
    Second part from the second homework from C#

---
+++ CSharp Homework 02-2/CSharp Homework 02-2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace CSharp_Homework_02_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number from 1 to 3!");
+            int parsedNumber;
+            bool selectedNumber = Int32.TryParse(Console.ReadLine(), out parsedNumber);
+
+            if (selectedNumber)
+            {
+                if (parsedNumber > 3 || parsedNumber < 1)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("ERROR: You can't enter a number higher then 3 or lower then 1!");
+                }
+                else 
+                {
+                    switch (parsedNumber)
+                    {
+                        case 1:
+                            Console.WriteLine("You got a new car!");
+                            break;
+                        case 2:
+                            Console.WriteLine("You got a new plane!");
+                            break;
+                        case 3:
+                            Console.WriteLine("You got a new bike!");
+
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("You can't enter characters other then numbers!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
