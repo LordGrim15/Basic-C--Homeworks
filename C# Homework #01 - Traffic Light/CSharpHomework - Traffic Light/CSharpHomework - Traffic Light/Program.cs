7d33a34c2ba34440cefcc9bba5bf9cea5c67957a
Petar Gjorgjioski <58377922+LordGrim15@users.noreply.github.com>
2020-03-08 04:00:09 +0100

    update

---
+++ C# Homework #01 - Traffic Light/CSharpHomework - Traffic Light/CSharpHomework - Traffic Light/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace HomeWork_1_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("STOP!");
+            Thread.Sleep(4000);
+            Console.BackgroundColor = ConsoleColor.DarkYellow;
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("READY");
+            Thread.Sleep(700);
+            Console.WriteLine(".");
+            Thread.Sleep(700);
+            Console.WriteLine(".");
+            Thread.Sleep(700);
+            Console.WriteLine(".");
+            Thread.Sleep(700);
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("GO!");
+            Console.ReadLine();
+        }
+    }
+}
