42432b501ed26d24961011619070386cbdc4c1d4
Petar Gjorgjioski <58377922+LordGrim15@users.noreply.github.com>
2020-03-13 17:21:25 +0100

    Break Exercise1

---
+++ Break Exercise1/Break Exercise1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Break_Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool parsedVal;
+
+            Console.WriteLine("Please enter true or false!");
+            bool inputVal = bool.TryParse(Console.ReadLine(), out parsedVal);
+
+            while (!inputVal)
+            {
+                Console.WriteLine("Invalid input! Please try again!");
+                inputVal = bool.TryParse(Console.ReadLine(), out parsedVal);
+            }
+
+            switch (parsedVal)
+            {
+                case true:
+                    Console.WriteLine("false");
+                    break;
+                case false:
+                    Console.WriteLine("true");
+                    break;
+                default:
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
